f35c7d35da7dd77c76aca42efdc3754d547e8d4f
KostyaD <thedamaxstudio@gmail.com>
2015-03-26 19:19:39 +0300

    prototype work

--- public/theme/app/scripts/main.js
+++ public/theme/app/scripts/main.js
@@ -147,6 +147,71 @@ Dictionary.KSP = [
         ]
     }
 ];
+Dictionary.NoScreen = [
+    {
+        id: 1,
+        name: "Динамика",
+        ksp: [
+            { id: 1 },
+            { id: 2 },
+            { id: 3 }
+        ]
+    },
+    {
+        id: 2,
+        name: "Дизайн",
+        ksp: [
+            { id: 1 },
+            { id: 2 },
+            { id: 3 }
+        ]
+    },
+    {
+        id: 3,
+        name: "Иновации",
+        ksp: [
+            { id: 1 },
+            { id: 2 },
+            { id: 3 }
+        ]
+    },
+    {
+        id: 4,
+        name: "Комфорт",
+        ksp: [
+            { id: 1 },
+            { id: 2 },
+            { id: 3 }
+        ]
+    },
+    {
+        id: 5,
+        name: "Экономичность",
+        ksp: [
+            { id: 1 },
+            { id: 2 },
+            { id: 3 }
+        ]
+    },
+    {
+        id: 6,
+        name: "Безопасность",
+        ksp: [
+            { id: 1 },
+            { id: 2 },
+            { id: 3 }
+        ]
+    },
+    {
+        id: 7,
+        name: "Функциональность",
+        ksp: [
+            { id: 1 },
+            { id: 2 },
+            { id: 3 }
+        ]
+    }
+];
 
 Help.Transform = function(transform_value) {
     var pref = ['', '-webkit-', '-ms-'];
@@ -161,30 +226,48 @@ Camry.answer = function() {
         link_no = $('.js-answer-no'),
         block_yes = $('.js-yes-block'),
         block_no = $('.js-no-block');
+        back_yes = $('.js-yes-back');
+        back_no = $('.js-no-back');
     var showYes = function() {
         block_yes.addClass('active');
+        $('.js-yes-screen').show();
+        back_no.hide();
         block_no.addClass('hide');
         setTimeout(function(){
             $('.js-background').fadeOut();
             $('.js-main-screen').fadeOut();
-            $('.yes-screen').addClass('active');
+            $('.js-yes-screen').addClass('active');
+        }, 1000);
+    }
+    var showNo = function() {
+        block_no.addClass('active');
+        $('.js-no-screen').show();
+        back_yes.hide();
+        block_yes.addClass('hide');
+        setTimeout(function(){
+            $('.js-background').fadeOut();
+            $('.js-main-screen').fadeOut();
+            $('.js-no-screen').addClass('active');
         }, 1000);
     }
     var init = function() {
+        $('.js-yes-screen, .js-no-screen').hide();
         link_yes.on('click', function(){
             showYes();
+        });
+        link_no.on('click', function(){
+            showNo();
         });
     }
     init();
 }
 Camry.yesScreen = function() {
     var self = this,
-        parent = $('.yes-screen'),
-        yesCont = $('.js-yes-cont'),
-        yesBlock = $('.js-yes-list'),
+        parent = $('.js-yes-screen'),
+        yesCont = parent.find('.js-yes-cont'),
+        yesBlock = parent.find('.js-yes-list'),
         wrapper = $('.wrapper'),
-        opened = false,
-        items_length = $('.js-yes-option').length;
+        opened = false;
     var csizes = {
             height: 0,
             width: 181
@@ -230,7 +313,7 @@ Camry.yesScreen = function() {
         for(var i = 0; i < obj.length; i++) {
             html += '<div class="yes-screen__option empty-option-after"></div>';
         }
-        $('.js-yes-cont').html(html);
+        $('.js-yes-screen .js-yes-cont').html(html);
     }
     this.start = function() {
         var new_bottom = -262/16;
@@ -238,7 +321,7 @@ Camry.yesScreen = function() {
     }
     this.kspNav = function() {
         var addKsp = function(id) {
-            var this_block = $('.js-yes-option[data-id="' + id + '"]'),
+            var this_block = $('.js-yes-screen .js-yes-option[data-id="' + id + '"]'),
                 this_ksp = this_block.find('.js-ksp');
             if(!this_ksp.filter('.opened').length) {
                 var this_item = this_ksp.last();
@@ -272,13 +355,17 @@ Camry.yesScreen = function() {
             setNav(this_block);
         }
         var deleteKsp = function(id) {
-            var this_block = $('.js-yes-option[data-id="' + id + '"]');
+            var this_block = $('.js-yes-screen .js-yes-option[data-id="' + id + '"]');
             var this_ksp = this_block.find('.js-ksp');
+            var this_background = this_block.find('.main-item__background').last().removeClass('active');
             this_ksp.filter('.opened').first().removeClass('opened');
+            setTimeout(function(){
+                this_background.remove();
+            }, 500);
             setNav(this_block);
         }
         var setNav = function(this_block) {
-            $('.js-yes-option').each(function(){
+            $('.js-yes-screen .js-yes-option').each(function(){
                 if($(this).find('.js-ksp.opened').length) {
                     $(this).find('.js-ksp-remove').addClass('opened');
                     if($(this).find('.js-ksp.opened').length == $(this).find('.js-ksp').length) {
@@ -314,15 +401,15 @@ Camry.yesScreen = function() {
             time1 = 500;
             time2 = 350;
         }
-        $('.empty-option-before, .empty-option-after').removeClass('show');
+        $('.js-yes-screen .empty-option-before, .empty-option-after').removeClass('show');
         var show_empty = (3 - td_id) * 2;
         if(show_empty > 0) {
             for(var i = 0; i < show_empty; i++)
-                $('.empty-option-before').eq(i).addClass('show');
+                $('.js-yes-screen .empty-option-before').eq(i).addClass('show');
         } else {
             show_empty = show_empty * (-1);
             for(var i = 0; i < show_empty; i++)
-                $('.empty-option-after').eq(i).addClass('show');
+                $('.js-yes-screen .empty-option-after').eq(i).addClass('show');
         }
         setTimeout(function(){
             var new_bottom = -(wrapper.height() - (osizes.height)) / 2 + tr_id * osizes.height;
@@ -349,7 +436,123 @@ Camry.yesScreen = function() {
     self.init();
 }
 
+Camry.noScreen = function() {
+    var self = this,
+        parent = $('.js-no-screen'),
+        yesCont = parent.find('.js-yes-cont'),
+        yesBlock = parent.find('.js-yes-list'),
+        wrapper = $('.wrapper'),
+        opened = false;
+    var csizes = {
+            height: 0,
+            width: 181
+        };
+    var osizes = {
+            height: 360,
+            width: 610
+        };
+    this.setHtml = function() {
+        var obj = Dictionary.NoScreen;
+        var dataid = 0;
+        var html = '';
+        for(var i = 0; i < obj.length; i++) {
+            html += '<div class="yes-screen__option empty-option-before"></div>';
+        }
+        $.each(obj, function(index, value){
+            var options_str = '';
+            var ksp_step = value.ksp.length - 1;
+            $.each(value.ksp, function(ksp_index, ksp_value){
+                options_str += '\
+                    <div data-td="' + dataid + '" data-tr="' + ksp_step + '" class="list__other-item js-ksp" data-option-id="' + value.id + '" data-ksp-id="' + ksp_value.id + '">\
+                      <div class="other-item__line"></div>\
+                    </div>';
+                ksp_step--;
+            });
+            html += '<div data-id="' + dataid + '" class="yes-screen__option js-yes-option">\
+              <div class="yes-screen__cont">\
+                <div class="list__other-items">\
+                  ' + options_str + '\
+                </div>\
+                <div class="list__option-name">' + value.name + '</div>\
+                <div class="list__nav"><a href="#" class="nav__minus js-ksp-remove"><span>-</span></a><a href="#" class="nav__plus js-ksp-add opened"><span>+</span></a></div>\
+              </div>\
+            </div>';
+            dataid++;
+        });
+        for(var i = 0; i < obj.length; i++) {
+            html += '<div class="yes-screen__option empty-option-after"></div>';
+        }
+        $('.js-no-screen .js-yes-cont').html(html);
+    }
+    this.start = function() {
+        var new_bottom = -262/16;
+        yesCont.attr('style', Help.Transform('translateY(' + new_bottom + 'rem)'));
+    }
+    this.kspNav = function() {
+        var addKsp = function(id) {
+            var this_block = $('.js-no-screen .js-yes-option[data-id="' + id + '"]'),
+                this_ksp = this_block.find('.js-ksp');
+            if(!this_ksp.filter('.opened').length) {
+                var this_item = this_ksp.last();
+                this_item.addClass('opened');
+            } else {
+                var this_item = this_ksp.filter('.opened').first().prev();
+                this_item.addClass('opened');
+            }
+            setNav(this_block);
+        }
+        var deleteKsp = function(id) {
+            var this_block = $('.js-no-screen .js-yes-option[data-id="' + id + '"]');
+            var this_ksp = this_block.find('.js-ksp');
+            this_ksp.filter('.opened').first().removeClass('opened');
+            setNav(this_block);
+        }
+        var setNav = function(this_block) {
+            if($('.js-no-screen .js-ksp').hasClass('opened')){
+                $('.js-no-submit').removeAttr('disabled');
+                $('.js-no-submit').removeAttr('onclick');
+            } else {
+                $('.js-no-submit').attr('onclick', 'return false;');
+                $('.js-no-submit').attr('disabled', 'disabled');
+            }
+            $('.js-no-screen .js-yes-option').each(function(){
+                if($(this).find('.js-ksp.opened').length) {
+                    $(this).find('.js-ksp-remove').addClass('opened');
+                    if($(this).find('.js-ksp.opened').length == $(this).find('.js-ksp').length) {
+                        $(this).find('.js-ksp-add').removeClass('opened');
+                    } else {
+                        $(this).find('.js-ksp-add').addClass('opened');
+                    }
+                } else {
+                    $(this).find('.js-ksp-add').addClass('opened');
+                    $(this).find('.js-ksp-remove').removeClass('opened');
+                }
+            });
+        }
+        $(document).on('click', '.js-ksp-add', function(){
+            if(!$(this).hasClass('opened')) return false;
+            var id = $(this).parents('.js-yes-option').attr('data-id');
+            addKsp(id);
+            return false;
+        });
+        $(document).on('click', '.js-ksp-remove', function(){
+            if(!$(this).hasClass('opened')) return false;
+            var id = $(this).parents('.js-yes-option').attr('data-id');
+            deleteKsp(id);
+            return false;
+        });
+        setNav();
+    }
+    this.init = function() {
+        self.setHtml();
+        self.start();
+        self.kspNav();
+    }
+    self.init();
+}
+
 $(function(){
     Camry.answer();
     Camry.yesScreen();
+    Camry.noScreen();
 });
