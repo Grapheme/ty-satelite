b9abff6506856fbb1b6577f7dc896f3c888031ec
KostyaD <thedamaxstudio@gmail.com>
2015-03-26 15:17:40 +0300

    yes page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,153 @@
 var Camry = {};
 var Help = {};
+var Dictionary = {};
+Dictionary.KSP = [
+    {
+        id: 500,
+        name: "Динамика",
+        image: "images/tmp/ksp1.jpg",
+        ksp: [
+            {
+                id: 1,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp2.jpg"
+            },
+            {
+                id: 2,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp3.jpg"
+            },
+            {
+                id: 3,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp4.jpg"
+            },
+            {
+                id: 4,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp1.jpg"
+            },
+        ]
+    },
+    {
+        id: 2,
+        name: "Дизайн",
+        image: "images/tmp/ksp2.jpg",
+        ksp: [
+            {
+                id: 1,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp3.jpg"
+            },
+            {
+                id: 2,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp4.jpg"
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Иновации",
+        image: "images/tmp/ksp3.jpg",
+        ksp: [
+            {
+                id: 1,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp1.jpg"
+            },
+            {
+                id: 2,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp2.jpg"
+            },
+            {
+                id: 3,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp3.jpg"
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: "Комфорт",
+        image: "images/tmp/ksp4.jpg",
+        ksp: [
+            {
+                id: 1,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp4.jpg"
+            },
+            {
+                id: 2,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp1.jpg"
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: "Экономичность",
+        image: "images/tmp/ksp1.jpg",
+        ksp: [
+            {
+                id: 1,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp2.jpg"
+            },
+            {
+                id: 2,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp3.jpg"
+            },
+            {
+                id: 3,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp4.jpg"
+            }
+        ]
+    },
+    {
+        id: 6,
+        name: "Безопасность",
+        image: "images/tmp/ksp2.jpg",
+        ksp: [
+            {
+                id: 1,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp1.jpg"
+            },
+            {
+                id: 2,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp2.jpg"
+            }
+        ]
+    },
+    {
+        id: 7,
+        name: "Функциональность",
+        image: "images/tmp/ksp3.jpg",
+        ksp: [
+            {
+                id: 1,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp3.jpg"
+            },
+            {
+                id: 2,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp4.jpg"
+            },
+            {
+                id: 3,
+                preview: "Новый двигатель 2.0 L с 6-ступенчатой АКПП",
+                image: "images/tmp/ksp1.jpg"
+            }
+        ]
+    }
+];
+
 Help.Transform = function(transform_value) {
     var pref = ['', '-webkit-', '-ms-'];
     var str = '';
@@ -8,7 +156,6 @@ Help.Transform = function(transform_value) {
     });
     return str;
 }
-
 Camry.answer = function() {
     var link_yes = $('.js-answer-yes'),
         link_no = $('.js-answer-no'),
@@ -39,21 +186,60 @@ Camry.yesScreen = function() {
         opened = false,
         items_length = $('.js-yes-option').length;
     var csizes = {
-        height: 360,
-        width: 160
-    };
+            height: 0,
+            width: 181
+        };
     var osizes = {
-        height: 360,
-        width: 610
-    };
+            height: 360,
+            width: 610
+        };
+    this.setHtml = function() {
+        var obj = Dictionary.KSP;
+        var dataid = 0;
+        var html = '';
+        for(var i = 0; i < obj.length; i++) {
+            html += '<div class="yes-screen__option empty-option-before"></div>';
+        }
+        $.each(obj, function(index, value){
+            var options_str = '';
+            var ksp_step = value.ksp.length - 1;
+            $.each(value.ksp, function(ksp_index, ksp_value){
+                options_str += '\
+                    <div data-td="' + dataid + '" data-tr="' + ksp_step + '" class="list__other-item js-ksp" data-option-id="' + value.id + '" data-ksp-id="' + ksp_value.id + '">\
+                      <div class="other-item__content">\
+                        <div class="other-item__background" style="background-image: url(' + ksp_value.image + ');">\
+                          <div class="background__text">' + ksp_value.preview + '</div>\
+                        </div>\
+                      </div>\
+                      <div class="other-item__line"></div>\
+                    </div>';
+                ksp_step--;
+            });
+            html += '<div data-id="' + dataid + '" class="yes-screen__option js-yes-option">\
+              <div class="yes-screen__cont">\
+                <div class="list__other-items">\
+                  ' + options_str + '\
+                </div>\
+                <div class="list__main-item" style="background-image: url(' + value.image + ');"></div>\
+                <div class="list__option-name">' + value.name + '</div>\
+                <div class="list__nav"><a href="#" class="nav__minus js-ksp-remove"><span>-</span></a><a href="#" class="nav__plus js-ksp-add opened"><span>+</span></a></div>\
+              </div>\
+            </div>';
+            dataid++;
+        });
+        for(var i = 0; i < obj.length; i++) {
+            html += '<div class="yes-screen__option empty-option-after"></div>';
+        }
+        $('.js-yes-cont').html(html);
+    }
     this.start = function() {
-        var new_bottom = wrapper.height()/100 * 10;
-        yesCont.attr('style', Help.Transform('translateY(' + new_bottom * (-1) + 'px)'));
+        var new_bottom = -262/16;
+        yesCont.attr('style', Help.Transform('translateY(' + new_bottom + 'rem)'));
     }
     this.kspNav = function() {
         var addKsp = function(id) {
-            var this_block = $('.js-yes-option[data-id="' + id + '"]');
-            var this_ksp = this_block.find('.js-ksp');
+            var this_block = $('.js-yes-option[data-id="' + id + '"]'),
+                this_ksp = this_block.find('.js-ksp');
             if(!this_ksp.filter('.opened').length) {
                 var this_item = this_ksp.last();
                 this_item.addClass('opened');
@@ -62,18 +248,36 @@ Camry.yesScreen = function() {
                 this_item.addClass('opened');
             }
             this_item.addClass('active');
+            var option_id = this_ksp.attr('data-option-id');
+            var ksp_id = this_ksp.filter('.opened').first().attr('data-ksp-id');
+            var background_new;
+            $.each(Dictionary.KSP, function(index, value){
+                if(value.id == option_id) {
+                    $.each(value.ksp, function(index, value){
+                        if(value.id == ksp_id) {
+                            console.log(value);
+                            background_new = value.image;
+                        }
+                    });
+                }
+            });
+            var background_block = $('<div class="main-item__background" style="background-image: url(' + background_new + ');"></div>');
+            this_block.find('.list__main-item').append(background_block);
             setTimeout(function(){
                 this_item.removeClass('active');
+                setTimeout(function(){
+                    background_block.addClass('active');
+                }, 500);
             }, 500);
-            setNav();
+            setNav(this_block);
         }
         var deleteKsp = function(id) {
             var this_block = $('.js-yes-option[data-id="' + id + '"]');
             var this_ksp = this_block.find('.js-ksp');
             this_ksp.filter('.opened').first().removeClass('opened');
-            setNav();
+            setNav(this_block);
         }
-        var setNav = function() {
+        var setNav = function(this_block) {
             $('.js-yes-option').each(function(){
                 if($(this).find('.js-ksp.opened').length) {
                     $(this).find('.js-ksp-remove').addClass('opened');
@@ -89,13 +293,13 @@ Camry.yesScreen = function() {
             });
         }
         $(document).on('click', '.js-ksp-add', function(){
-            if(!$(this).hasClass('opened')) return;
+            if(!$(this).hasClass('opened')) return false;
             var id = $(this).parents('.js-yes-option').attr('data-id');
             addKsp(id);
             return false;
         });
         $(document).on('click', '.js-ksp-remove', function(){
-            if(!$(this).hasClass('opened')) return;
+            if(!$(this).hasClass('opened')) return false;
             var id = $(this).parents('.js-yes-option').attr('data-id');
             deleteKsp(id);
             return false;
@@ -137,6 +341,7 @@ Camry.yesScreen = function() {
         })
     }
     this.init = function() {
+        self.setHtml();
         self.start();
         self.setEvents();
         self.kspNav();
